0996d07062390cf2910bcf8fd76d402745f9c2fe
Evan Martinez <evanm.design@gmail.com>
2018-01-20 17:07:57 -0500

    crypto code challenge functional

--- app/controllers/courses/coding/mining.js
+++ app/controllers/courses/coding/mining.js
@@ -1,30 +1,40 @@
 import Controller from '@ember/controller';
+import crypto from 'npm:crypto-js';
 
 export default Controller.extend({
-	acceptableAnswers: ['abc', 'def'],
-	testCases: [
-		{
-			input: 123,
-			expected: 'ABC'
-		},
-		{
-			input: 456,
-			expected: 'DEF'
-		}
-	],
 	customCode: '',
 
 	modifyCode(customCode) {
-		return `let privateKey = arguments[0]; ${customCode}`;
+		return `const crypto = arguments[1]; let privateKey = arguments[0]; ${customCode}`;
+	},
+
+	generateTestCases(numDesired) {
+		let testCases = [];
+
+		for (var i = 0; i < numDesired; i++) {
+			let privateKey = `test__${i}`;
+			let modifiedPrivateKey = `0x80${privateKey}`;
+			let hashed = crypto.SHA256(modifiedPrivateKey);
+			let doubleHashed = crypto.SHA256(hashed)
+
+			testCases.push({
+				input: privateKey,
+				expected: doubleHashed.toString()
+			});
+		}
+
+		return testCases;
 	},
 
 	actions: {
 		executeCustomCode() {
-			this.get('testCases').forEach((testCase) => {
+			let testCases = this.generateTestCases(3);
+
+			testCases.forEach((testCase) => {
 				let modifiedCode = this.modifyCode(this.get('customCode'));
 
-				let result = new Function(modifiedCode)(testCase.input);
-				if (result === testCase.expected) {
+				let result = new Function(modifiedCode)(testCase.input, crypto);
+				if (result == testCase.expected) {
 					alert('you got it right!');
 				} else {
 					alert(`you returned ${result} and we expected ${testCase.expected}`);
